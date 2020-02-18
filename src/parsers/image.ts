6d5ae6c2b4b5dc593fd2385db8bc79d3adda2cfc
Evan Yamanishi <yamanishi1@gmail.com>
2020-02-17 19:50:46 -0500

    refactor: simplify image parser

--- src/parsers/image.ts
+++ src/parsers/image.ts
@@ -1,58 +1,26 @@
 import { getAllDescriptions, DescriptionRecord } from './description';
 
-export interface ImageDetails {
+export interface ImageContents {
 	alt: string | null;
 	hasLongDescription: boolean;
 	descriptions: Partial<DescriptionRecord>;
 }
 
-export interface ImageParserOptions {
-	validateAlt?: boolean;
-}
-
-class ImageParser {
-	public options: Required<ImageParserOptions>;
-
-	constructor({
-		validateAlt = true,
-	}: ImageParserOptions = {}) {
-		this.options = {
-			validateAlt,
-		};
-	}
-
-	public async parse(image: HTMLImageElement): Promise<ImageDetails> {
-		const { validateAlt } = this.options;
-
-		const d = await getAllDescriptions(image);
-		const hasLongDescription = d.some(([, desc]) => desc);
-		const descriptions: Partial<DescriptionRecord> = {};
-		d.forEach(([attr, desc]) => {
-			if (desc) {
-				descriptions[attr] = desc;
-			}
-		});
-
-		const details: ImageDetails = {
-			alt: image.getAttribute('alt'),
-			hasLongDescription,
-			descriptions,
-		};
-
-		if (validateAlt) {
-			// TODO: implement alt text validation
+const parse = async (image: HTMLImageElement): Promise<ImageContents> => {
+	const d = await getAllDescriptions(image);
+	const hasLongDescription = d.some(([, desc]) => desc);
+	const descriptions: Partial<DescriptionRecord> = {};
+	d.forEach(([attr, desc]) => {
+		if (desc) {
+			descriptions[attr] = desc;
 		}
+	});
 
-		return details;
-	}
-}
-
-export const parse = (
-	image: HTMLImageElement,
-	options?: ImageParserOptions,
-): Promise<ImageDetails> => {
-	const parser = new ImageParser(options);
-	return parser.parse(image);
+	return {
+		alt: image.getAttribute('alt'),
+		hasLongDescription,
+		descriptions,
+	};
 };
 
-export default ImageParser;
+export default parse;
